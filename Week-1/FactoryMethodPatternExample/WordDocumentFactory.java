aa385899b28e4af1cc01701b356c96e9be64293c
sanya bekal <160402337+sanyabekal@users.noreply.github.com>
2025-06-20 18:12:27 +0530

    week-1 HOL

---
+++ Week-1/FactoryMethodPatternExample/WordDocumentFactory.java
@@ -0,0 +1,9 @@
+// WordDocumentFactory.java
+public class WordDocumentFactory extends DocumentFactory {
+    @Override
+    public Document createDocument() {
+        return new WordDocument();
+    }
+}
+
+
